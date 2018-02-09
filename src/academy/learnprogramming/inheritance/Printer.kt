f3cfb213cedf074be3dab0003dcf698ef29d886c
Robert Lowe <robertmlowe@rmlowe.com>
2018-02-09 14:07:23 +0800

    Initial commit

---
+++ src/academy/learnprogramming/inheritance/Printer.kt
@@ -0,0 +1,22 @@
+package academy.learnprogramming.inheritance
+
+
+fun main(args: Array<String>) {
+    val laserPrinter = LaserPrinter("Brother 1234")
+    laserPrinter.printModel()
+}
+
+
+abstract class Printer(val modelName: String) {
+
+    open fun printModel() = println("The model name of this printer is $modelName")
+    abstract fun bestSellingPrice(): Double
+
+}
+
+class LaserPrinter(modelName: String): Printer(modelName) {
+
+    override fun printModel() = println("The model name of this laser printer is $modelName")
+    override fun bestSellingPrice(): Double = 129.99
+
+}
